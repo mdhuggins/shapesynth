bceb89c0be3d04af5eed21e3d380695cc3aefc4c
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2019-05-06 00:03:41 -0400

    Improve cycling through HSV hues; improve backdrop during draw/edit

--- color.py
+++ color.py
@@ -12,16 +12,16 @@ class ColorPalette(object):
         self.hue_variance = 0.1
         self.hue_average = 0.5
         self.hue_range = (0, 1)
-        self.brightness_range = (0.5, 1)
+        self.brightness_range = (0.7, 1)
 
     def process_harmony(self, harmony):
         """
         Updates the hue range when the harmony changes.
         """
         # Sample hue using a normal distribution centered around major/minor balance
-        hue_average = np.mod(np.random.uniform(self.hue_average - 0.2, self.hue_average + 0.2), 1.0)
+        self.hue_average = np.mod(np.random.uniform(self.hue_average - 0.2, self.hue_average + 0.2), 2.0)
 
-        self.hue_range = (np.clip(hue_average - self.hue_variance, 0.0, 1.0), np.clip(hue_average + self.hue_variance, 0.0, 1.0))
+        self.hue_range = (np.clip(self.hue_average - self.hue_variance, 0.0, 2.0), np.clip(self.hue_average + self.hue_variance, 0.0, 2.0))
         self.cache_harmony = harmony
 
     def new_color(self, saturation_parameter):
@@ -32,4 +32,4 @@ class ColorPalette(object):
         """
         if Conductor.harmony != self.cache_harmony:
             self.process_harmony(Conductor.harmony)
-        return (np.random.uniform(*self.hue_range), np.clip(np.random.normal(saturation_parameter * 2.0, 0.2), 0.0, 1.0), np.random.uniform(*self.brightness_range))
+        return (np.mod(np.random.uniform(*self.hue_range), 1.0), np.clip(np.random.normal(saturation_parameter * 2.0, 0.2), 0.0, 1.0), np.random.uniform(*self.brightness_range))
